35d587d51a5119d6f449996c7ef5c92f19367140
Igormandello <igormandello@gmail.com>
2018-03-19 11:47:59 -0300

    Creates Interfaces for DAOs

---
+++ rest-api/rest-api/DB/DAOs/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rest_api.DB.DAOs
+{
+    interface IRepository<X>
+    {
+        List<X> GetAll();
+        X GetByID(int id);
+        X Insert(X item);
+        void Delete(int id);
+        void Update(X item);
+    }
+}
