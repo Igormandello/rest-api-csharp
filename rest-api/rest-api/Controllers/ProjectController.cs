ee75f69e2bead18100712b03a665d642df753fd6
Igormandello <igormandello@gmail.com>
2018-03-19 13:26:31 -0300

    Adds ProjectController
    - Sets the routes to project
    
    - Creates non-implemented methods for the controller

---
+++ rest-api/rest-api/Controllers/ProjectController.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using rest_api.DB;
+using rest_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Web.Http;
+
+namespace rest_api.Controllers
+{
+    [RoutePrefix("api/project")]
+    public class ProjectController : ApiController
+    {
+        [AcceptVerbs("POST")]
+        [Route("create")]
+        public String Create(Project project)
+        {
+            return "";
+        }
+
+        [AcceptVerbs("PUT")]
+        [Route("{id}/update")]
+        public String Update(Project project)
+        {
+            return "";
+        }
+
+        [AcceptVerbs("DELETE")]
+        [Route("{id}/delete")]
+        public String Delete(int id)
+        {
+            return "";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("{id}")]
+        public String Select(int id)
+        {
+            return "{}";
+        }
+
+        [AcceptVerbs("GET")]
+        [Route("")]
+        public String SelectAll()
+        {
+            return "{}";
+        }
+    }
+}
