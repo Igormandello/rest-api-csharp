333815f5478ac6c2f3cabfabe43a627c0afdb16e
Igormandello <igormandello@gmail.com>
2018-03-19 11:23:55 -0300

    Adds Project Model

---
+++ rest-api/rest-api/Models/Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rest_api.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public int Year { get; set; }
+
+
+        public Project(int id, String name, String desc, int year)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = desc;
+            this.Year = year;
+        }
+    }
+}
