a973d993044ef81ab9474214a05f3e08e0828558
Igormandello <igormandello@gmail.com>
2018-03-19 11:23:29 -0300

    Creates Student Model

---
+++ rest-api/rest-api/Models/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rest_api.Models
+{
+    public class Student
+    {
+        public int RA { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+
+        public Student(int ra, String name, String email)
+        {
+            this.RA = ra;
+            this.Name = name;
+            this.Email = email;
+        }
+    }
+}
