662de201f8919617220c03592b7ff6ca589b00d4
Igormandello <igormandello@gmail.com>
2018-03-19 11:29:53 -0300

    Creates a wrapper class for DB connection
    - DBConnection stores a static connection that will be used for all DAOs

---
+++ rest-api/rest-api/DB/DBConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace rest_api.DB
+{
+    public class DBConnection
+    {
+        private static SqlConnection con;
+
+        public DBConnection(String conStr)
+        {
+            con = new SqlConnection(conStr);
+            con.Open();
+        }
+
+        public static object ExecuteScalar(String sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, con);
+            return cmd.ExecuteReader();
+        }
+
+        public static SqlDataReader ExecuteReader(String sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, con);
+            return cmd.ExecuteReader();
+        }
+
+        public static void ExecuteNonQuery(String sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
