f11912d918109a749d965781b7b9a18d151dfe30
Igormandello <igormandello@gmail.com>
2018-03-19 13:23:15 -0300

    Adds base file to ProjectDAO

---
+++ rest-api/rest-api/DB/DAOs/ProjectDAO.cs
@@ -0,0 +1,36 @@
+﻿using rest_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rest_api.DB.DAOs
+{
+    public class ProjectDAO : IRepository<Project>
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Project> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project Insert(Project item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Project item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
