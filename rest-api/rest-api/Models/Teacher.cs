e68d5a74bf4c354ecf2ddb739801247915651aa5
Igormandello <igormandello@gmail.com>
2018-03-19 11:22:56 -0300

    Creates Teacher Model

---
+++ rest-api/rest-api/Models/Teacher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace rest_api.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+
+        public Teacher(int id, String name, String email)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+        }
+    }
+}
