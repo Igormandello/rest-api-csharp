164702aba0b0f6429965dd7587200617cea4d8ce
Igormandello <igormandello@gmail.com>
2018-03-22 11:11:56 -0300

    Fixes the null key issue
    - Now the key is a string, and always is tryed to parse it to a int, if it's possible parse, GetById is called, if it isn't possible, GetByName is called

--- rest-api/rest-api/Controllers/ProjectController.cs
+++ rest-api/rest-api/Controllers/ProjectController.cs
@@ -33,13 +33,9 @@ namespace rest_api.Controllers
 
         [AcceptVerbs("GET")]
         [Route("{key}")]
-        public Project Select(object key)
+        public Project Select(string key)
         {
-            
-            if (key.GetType().Equals(Type.GetType("int")))
-            {
-                int id = (int)key;
-
+            if (Int32.TryParse(key, out int id))
                 try
                 {
                     return dao.GetByID(id);
@@ -48,22 +44,15 @@ namespace rest_api.Controllers
                 {
                     return null;
                 }
-            }
             else
-            {
-                string name = (string)key;
-
                 try
                 {
-                    return dao.GetByName(name);
+                    return dao.GetByName(key);
                 }
                 catch
                 {
                     return null;
                 }
-            }
-                
-
         }
 
         [AcceptVerbs("POST")]
